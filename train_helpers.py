6ccfed81828b7fbdea7e630e7071e48bdc963b61
Fabrice Normandin <normandf@mila.quebec>
2025-09-24 12:11:19 -0400

    Add job.sh script and replace MPI with pure slurm
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- train_helpers.py
+++ train_helpers.py
@@ -1,20 +1,15 @@
 import torch
 import numpy as np
-from mpi4py import MPI
-import socket
 import argparse
 import os
 import json
 import subprocess
 from hps import Hyperparams, parse_args_and_update_hparams, add_vae_arguments
-from utils import (logger,
-                   local_mpi_rank,
-                   mpi_size,
-                   maybe_download,
-                   mpi_rank)
+from utils import logger, maybe_download
 from data import mkdir_p
 from contextlib import contextmanager
 import torch.distributed as dist
+
 # from apex.optimizers import FusedAdam as AdamW
 from torch.optim import AdamW
 from vae import VAE
@@ -28,33 +23,37 @@ def update_ema(vae, ema_vae, ema_rate):
 
 
 def save_model(path, vae, ema_vae, optimizer, H):
-    torch.save(vae.state_dict(), f'{path}-model.th')
-    torch.save(ema_vae.state_dict(), f'{path}-model-ema.th')
-    torch.save(optimizer.state_dict(), f'{path}-opt.th')
-    from_log = os.path.join(H.save_dir, 'log.jsonl')
-    to_log = f'{os.path.dirname(path)}/{os.path.basename(path)}-log.jsonl'
-    subprocess.check_output(['cp', from_log, to_log])
+    torch.save(vae.state_dict(), f"{path}-model.th")
+    torch.save(ema_vae.state_dict(), f"{path}-model-ema.th")
+    torch.save(optimizer.state_dict(), f"{path}-opt.th")
+    from_log = os.path.join(H.save_dir, "log.jsonl")
+    to_log = f"{os.path.dirname(path)}/{os.path.basename(path)}-log.jsonl"
+    subprocess.check_output(["cp", from_log, to_log])
 
 
 def accumulate_stats(stats, frequency):
     z = {}
     for k in stats[-1]:
-        if k in ['distortion_nans', 'rate_nans', 'skipped_updates', 'gcskip']:
+        if k in ["distortion_nans", "rate_nans", "skipped_updates", "gcskip"]:
             z[k] = np.sum([a[k] for a in stats[-frequency:]])
-        elif k == 'grad_norm':
+        elif k == "grad_norm":
             vals = [a[k] for a in stats[-frequency:]]
             finites = np.array(vals)[np.isfinite(vals)]
             if len(finites) == 0:
                 z[k] = 0.0
             else:
                 z[k] = np.max(finites)
-        elif k == 'elbo':
+        elif k == "elbo":
             vals = [a[k] for a in stats[-frequency:]]
             finites = np.array(vals)[np.isfinite(vals)]
-            z['elbo'] = np.mean(vals)
-            z['elbo_filtered'] = np.mean(finites)
-        elif k == 'iter_time':
-            z[k] = stats[-1][k] if len(stats) < frequency else np.mean([a[k] for a in stats[-frequency:]])
+            z["elbo"] = np.mean(vals)
+            z["elbo_filtered"] = np.mean(finites)
+        elif k == "iter_time":
+            z[k] = (
+                stats[-1][k]
+                if len(stats) < frequency
+                else np.mean([a[k] for a in stats[-frequency:]])
+            )
         else:
             z[k] = np.mean([a[k] for a in stats[-frequency:]])
     return z
@@ -63,26 +62,59 @@ def accumulate_stats(stats, frequency):
 def linear_warmup(warmup_iters):
     def f(iteration):
         return 1.0 if iteration > warmup_iters else iteration / warmup_iters
+
     return f
 
 
 def setup_mpi(H):
-    H.mpi_size = mpi_size()
-    H.local_rank = local_mpi_rank()
-    H.rank = mpi_rank()
-    os.environ["RANK"] = str(H.rank)
-    os.environ["WORLD_SIZE"] = str(H.mpi_size)
-    os.environ["MASTER_PORT"] = str(H.port)
-    # os.environ["NCCL_LL_THRESHOLD"] = "0"
-    os.environ["MASTER_ADDR"] = MPI.COMM_WORLD.bcast(socket.gethostname(), root=0)
+    # Old, mpi-style approach:
+    # H.mpi_size = mpi_size()
+    # H.local_rank = local_mpi_rank()
+    # H.rank = mpi_rank()
+    # os.environ["RANK"] = str(H.rank)
+    # os.environ["WORLD_SIZE"] = str(H.mpi_size)
+    # os.environ["MASTER_PORT"] = str(H.port)
+    # # os.environ["NCCL_LL_THRESHOLD"] = "0"
+    # os.environ["MASTER_ADDR"] = MPI.COMM_WORLD.bcast(socket.gethostname(), root=0)
+    # dist.init_process_group(backend="nccl", init_method=f"env://")
+
+    # SLURM-based approach
+    H.mpi_size = WORLD_SIZE = int(
+        os.environ.setdefault("WORLD_SIZE", os.environ.get("SLURM_NTASKS", "1"))
+    )
+    H.local_rank = LOCAL_RANK = int(
+        os.environ.setdefault("LOCAL_RANK", os.environ.get("SLURM_LOCALID", "0"))
+    )
+    H.rank = RANK = int(
+        os.environ.setdefault("RANK", os.environ.get("SLURM_PROCID", "0"))
+    )
+    H.port = MASTER_PORT = int(
+        os.environ.setdefault(
+            "MASTER_PORT", str(10_000 + int(os.environ["SLURM_JOB_ID"]) % 10_000)
+        )
+    )
+    _first_node = subprocess.check_output(
+        f"scontrol show hostnames {os.environ['SLURM_JOB_NODELIST']}",
+        text=True,
+        shell=True,
+    ).split()[0]
+    MASTER_ADDR = os.environ.setdefault("MASTER_ADDR", _first_node)
+    # Get the hostname of the first node, for example: "cn-l[084-085]" --> cn-l084
+    # MASTER_ADDR = os.environ.setdefault("MASTER_ADDR", _first_node)
     torch.cuda.set_device(H.local_rank)
-    dist.init_process_group(backend='nccl', init_method=f"env://")
+    dist.init_process_group(
+        backend="nccl",
+        rank=RANK,
+        world_size=WORLD_SIZE,
+        init_method=f"tcp://{MASTER_ADDR}:{MASTER_PORT}",
+        # timeout=datetime.timedelta(minutes=5),
+    )
 
 
 def distributed_maybe_download(path, local_rank, mpi_size):
-    if not path.startswith('gs://'):
+    if not path.startswith("gs://"):
         return path
-    filename = path[5:].replace('/', '-')
+    filename = path[5:].replace("/", "-")
     with first_rank_first(local_rank, mpi_size):
         fp = maybe_download(path, filename)
     return fp
@@ -103,7 +135,7 @@ def first_rank_first(local_rank, mpi_size):
 def setup_save_dirs(H):
     H.save_dir = os.path.join(H.save_dir, H.desc)
     mkdir_p(H.save_dir)
-    H.logdir = os.path.join(H.save_dir, 'log')
+    H.logdir = os.path.join(H.save_dir, "log")
 
 
 def set_up_hyperparams(s=None):
@@ -115,21 +147,24 @@ def set_up_hyperparams(s=None):
     setup_save_dirs(H)
     logprint = logger(H.logdir)
     for i, k in enumerate(sorted(H)):
-        logprint(type='hparam', key=k, value=H[k])
+        logprint(type="hparam", key=k, value=H[k])
     np.random.seed(H.seed)
     torch.manual_seed(H.seed)
     torch.cuda.manual_seed(H.seed)
-    logprint('training model', H.desc, 'on', H.dataset)
+    logprint("training model", H.desc, "on", H.dataset)
     return H, logprint
 
 
 def restore_params(model, path, local_rank, mpi_size, map_ddp=True, map_cpu=False):
-    state_dict = torch.load(distributed_maybe_download(path, local_rank, mpi_size), map_location='cpu' if map_cpu else None)
+    state_dict = torch.load(
+        distributed_maybe_download(path, local_rank, mpi_size),
+        map_location="cpu" if map_cpu else None,
+    )
     if map_ddp:
         new_state_dict = {}
-        l = len('module.')
+        l = len("module.")
         for k in state_dict:
-            if k.startswith('module.'):
+            if k.startswith("module."):
                 new_state_dict[k[l:]] = state_dict[k]
             else:
                 new_state_dict[k] = state_dict[k]
@@ -138,26 +173,47 @@ def restore_params(model, path, local_rank, mpi_size, map_ddp=True, map_cpu=Fals
 
 
 def restore_log(path, local_rank, mpi_size):
-    loaded = [json.loads(l) for l in open(distributed_maybe_download(path, local_rank, mpi_size))]
+    loaded = [
+        json.loads(l)
+        for l in open(distributed_maybe_download(path, local_rank, mpi_size))
+    ]
     try:
-        cur_eval_loss = min([z['elbo'] for z in loaded if 'type' in z and z['type'] == 'eval_loss'])
+        cur_eval_loss = min(
+            [z["elbo"] for z in loaded if "type" in z and z["type"] == "eval_loss"]
+        )
     except ValueError:
-        cur_eval_loss = float('inf')
-    starting_epoch = max([z['epoch'] for z in loaded if 'type' in z and z['type'] == 'train_loss'])
-    iterate = max([z['step'] for z in loaded if 'type' in z and z['type'] == 'train_loss'])
+        cur_eval_loss = float("inf")
+    starting_epoch = max(
+        [z["epoch"] for z in loaded if "type" in z and z["type"] == "train_loss"]
+    )
+    iterate = max(
+        [z["step"] for z in loaded if "type" in z and z["type"] == "train_loss"]
+    )
     return cur_eval_loss, iterate, starting_epoch
 
 
 def load_vaes(H, logprint):
     vae = VAE(H)
     if H.restore_path:
-        logprint(f'Restoring vae from {H.restore_path}')
-        restore_params(vae, H.restore_path, map_cpu=True, local_rank=H.local_rank, mpi_size=H.mpi_size)
+        logprint(f"Restoring vae from {H.restore_path}")
+        restore_params(
+            vae,
+            H.restore_path,
+            map_cpu=True,
+            local_rank=H.local_rank,
+            mpi_size=H.mpi_size,
+        )
 
     ema_vae = VAE(H)
     if H.restore_ema_path:
-        logprint(f'Restoring ema vae from {H.restore_ema_path}')
-        restore_params(ema_vae, H.restore_ema_path, map_cpu=True, local_rank=H.local_rank, mpi_size=H.mpi_size)
+        logprint(f"Restoring ema vae from {H.restore_ema_path}")
+        restore_params(
+            ema_vae,
+            H.restore_ema_path,
+            map_cpu=True,
+            local_rank=H.local_rank,
+            mpi_size=H.mpi_size,
+        )
     else:
         ema_vae.load_state_dict(vae.state_dict())
     ema_vae.requires_grad_(False)
@@ -165,27 +221,48 @@ def load_vaes(H, logprint):
     vae = vae.cuda(H.local_rank)
     ema_vae = ema_vae.cuda(H.local_rank)
 
-    vae = DistributedDataParallel(vae, device_ids=[H.local_rank], output_device=H.local_rank)
+    vae = DistributedDataParallel(
+        vae, device_ids=[H.local_rank], output_device=H.local_rank
+    )
 
     if len(list(vae.named_parameters())) != len(list(vae.parameters())):
-        raise ValueError('Some params are not named. Please name all params.')
+        raise ValueError("Some params are not named. Please name all params.")
     total_params = 0
     for name, p in vae.named_parameters():
         total_params += np.prod(p.shape)
-    logprint(total_params=total_params, readable=f'{total_params:,}')
+    logprint(total_params=total_params, readable=f"{total_params:,}")
     return vae, ema_vae
 
 
 def load_opt(H, vae, logprint):
-    optimizer = AdamW(vae.parameters(), weight_decay=H.wd, lr=H.lr, betas=(H.adam_beta1, H.adam_beta2))
-    scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=linear_warmup(H.warmup_iters))
+    optimizer = AdamW(
+        vae.parameters(), weight_decay=H.wd, lr=H.lr, betas=(H.adam_beta1, H.adam_beta2)
+    )
+    scheduler = torch.optim.lr_scheduler.LambdaLR(
+        optimizer, lr_lambda=linear_warmup(H.warmup_iters)
+    )
 
     if H.restore_optimizer_path:
         optimizer.load_state_dict(
-            torch.load(distributed_maybe_download(H.restore_optimizer_path, H.local_rank, H.mpi_size), map_location='cpu'))
+            torch.load(
+                distributed_maybe_download(
+                    H.restore_optimizer_path, H.local_rank, H.mpi_size
+                ),
+                map_location="cpu",
+            )
+        )
     if H.restore_log_path:
-        cur_eval_loss, iterate, starting_epoch = restore_log(H.restore_log_path, H.local_rank, H.mpi_size)
+        cur_eval_loss, iterate, starting_epoch = restore_log(
+            H.restore_log_path, H.local_rank, H.mpi_size
+        )
     else:
-        cur_eval_loss, iterate, starting_epoch = float('inf'), 0, 0
-    logprint('starting at epoch', starting_epoch, 'iterate', iterate, 'eval loss', cur_eval_loss)
+        cur_eval_loss, iterate, starting_epoch = float("inf"), 0, 0
+    logprint(
+        "starting at epoch",
+        starting_epoch,
+        "iterate",
+        iterate,
+        "eval loss",
+        cur_eval_loss,
+    )
     return optimizer, scheduler, cur_eval_loss, iterate, starting_epoch
--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from mpi4py import MPI
+# from mpi4py import MPI
 import os
 import json
 import tempfile
@@ -17,7 +17,12 @@ def allreduce(x, average):
 
 def get_cpu_stats_over_ranks(stat_dict):
     keys = sorted(stat_dict.keys())
-    allreduced = allreduce(torch.stack([torch.as_tensor(stat_dict[k]).detach().cuda().float() for k in keys]), average=True).cpu()
+    allreduced = allreduce(
+        torch.stack(
+            [torch.as_tensor(stat_dict[k]).detach().cuda().float() for k in keys]
+        ),
+        average=True,
+    ).cpu()
     return {k: allreduced[i].item() for (i, k) in enumerate(keys)}
 
 
@@ -33,18 +38,18 @@ class Hyperparams(dict):
 
 
 def logger(log_prefix):
-    'Prints the arguments out to stdout, .txt, and .jsonl files'
+    "Prints the arguments out to stdout, .txt, and .jsonl files"
 
-    jsonl_path = f'{log_prefix}.jsonl'
-    txt_path = f'{log_prefix}.txt'
+    jsonl_path = f"{log_prefix}.jsonl"
+    txt_path = f"{log_prefix}.txt"
 
     def log(*args, pprint=False, **kwargs):
         if mpi_rank() != 0:
             return
         t = time.ctime()
-        argdict = {'time': t}
+        argdict = {"time": t}
         if len(args) > 0:
-            argdict['message'] = ' '.join([str(x) for x in args])
+            argdict["message"] = " ".join([str(x) for x in args])
         argdict.update(kwargs)
 
         txt_str = []
@@ -59,9 +64,9 @@ def logger(log_prefix):
                 val = float(val)
             argdict[k] = val
             if isinstance(val, float):
-                val = f'{val:.5f}'
-            txt_str.append(f'{k}: {val}')
-        txt_str = ', '.join(txt_str)
+                val = f"{val:.5f}"
+            txt_str.append(f"{k}: {val}")
+        txt_str = ", ".join(txt_str)
 
         if pprint:
             json_str = json.dumps(argdict, sort_keys=True)
@@ -80,49 +85,52 @@ def logger(log_prefix):
 
 
 def maybe_download(path, filename=None):
-    '''If a path is a gsutil path, download it and return the local link,
-    otherwise return link'''
-    if not path.startswith('gs://'):
+    """If a path is a gsutil path, download it and return the local link,
+    otherwise return link"""
+    if not path.startswith("gs://"):
         return path
     if filename:
-        local_dest = f'/tmp/'
-        out_path = f'/tmp/{filename}'
+        local_dest = f"/tmp/"
+        out_path = f"/tmp/{filename}"
         if os.path.isfile(out_path):
             return out_path
-        subprocess.check_output(['gsutil', '-m', 'cp', '-R', path, out_path])
+        subprocess.check_output(["gsutil", "-m", "cp", "-R", path, out_path])
         return out_path
     else:
         local_dest = tempfile.mkstemp()[1]
-        subprocess.check_output(['gsutil', '-m', 'cp', path, local_dest])
+        subprocess.check_output(["gsutil", "-m", "cp", path, local_dest])
     return local_dest
 
 
 def tile_images(images, d1=4, d2=4, border=1):
     id1, id2, c = images[0].shape
-    out = np.ones([d1 * id1 + border * (d1 + 1),
-                   d2 * id2 + border * (d2 + 1),
-                   c], dtype=np.uint8)
+    out = np.ones(
+        [d1 * id1 + border * (d1 + 1), d2 * id2 + border * (d2 + 1), c], dtype=np.uint8
+    )
     out *= 255
     if len(images) != d1 * d2:
-        raise ValueError('Wrong num of images')
+        raise ValueError("Wrong num of images")
     for imgnum, im in enumerate(images):
         num_d1 = imgnum // d2
         num_d2 = imgnum % d2
         start_d1 = num_d1 * id1 + border * (num_d1 + 1)
         start_d2 = num_d2 * id2 + border * (num_d2 + 1)
-        out[start_d1:start_d1 + id1, start_d2:start_d2 + id2, :] = im
+        out[start_d1 : start_d1 + id1, start_d2 : start_d2 + id2, :] = im
     return out
 
 
 def mpi_size():
-    return MPI.COMM_WORLD.Get_size()
+    return torch.distributed.get_world_size()
+    # return MPI.COMM_WORLD.Get_size()
 
 
 def mpi_rank():
-    return MPI.COMM_WORLD.Get_rank()
+    return torch.distributed.get_rank()
+    # return MPI.COMM_WORLD.Get_rank()
 
 
 def num_nodes():
+    return int(os.environ.get("SLURM_STEP_NUM_NODES", "1"))
     nn = mpi_size()
     if nn % 8 == 0:
         return nn // 8
